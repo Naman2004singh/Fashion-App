694cb2d43a527b1c87b01b937a6dab0f1eb71e29
Naman2004singh <naman.2singh9@gmail.com>
2025-04-11 20:59:29 +0530

    added the basic project structure

---
+++ lib/config/constants/app_color.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static Color themeColor = const Color(0xff967259);
+  static Color blackColor = const Color(0xff000000);
+  static Color whiteColor = const Color(0xffffffff);
+  static Color lightColor = const Color(0xffECE0D1);
+}
---
+++ lib/config/constants/styles.dart
@@ -0,0 +1,10 @@
+import 'package:fashion_app/config/constants/app_color.dart';
+import 'package:flutter/material.dart';
+
+class Styles {
+  static TextStyle headlineLarge = TextStyle(
+      fontFamily: "Poppins",
+      fontSize: 28.0,
+      fontWeight: FontWeight.w600,
+      color: AppColor.blackColor);
+}
