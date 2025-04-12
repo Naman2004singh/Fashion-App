605360b1e08875759574402e3f10c7ef28f9f2bd
Naman2004singh <naman.2singh9@gmail.com>
2025-04-13 00:15:08 +0530

    updated the phone number state with bloc and implement validations

--- lib/views/registration/widgets/phone_no.dart
+++ lib/views/registration/widgets/phone_no.dart
@@ -1,5 +1,7 @@
 import 'package:fashion_app/utils/input_decoration.dart';
+import 'package:fashion_app/views/registration/widgets/common_exports.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class PhoneNo extends StatelessWidget {
   const PhoneNo({super.key, required this.phoneFocusNode});
@@ -8,12 +10,33 @@ class PhoneNo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      keyboardType: TextInputType.phone,
-      focusNode: phoneFocusNode,
-      decoration: CustomInputDecoration.getDecoration(hintText: "Phone Number"),
-      onChanged: (value) {},
-      validator: (value) {},
+    return BlocBuilder<RegisterBloc, RegisterStates>(
+      buildWhen: (previous, current) => previous.phoneNo != current.phoneNo,
+      builder: (context, state) {
+        return TextFormField(
+          keyboardType: TextInputType.phone,
+          focusNode: phoneFocusNode,
+          decoration:
+              CustomInputDecoration.getDecoration(hintText: "Phone Number"),
+          onChanged: (value) {
+            int? phoneNum = int.tryParse(value);
+            if (phoneNum != null) {
+              context
+                  .read<RegisterBloc>()
+                  .add(PhoneNoChanged(phoneNo: phoneNum));
+            }
+          },
+          validator: (value) {
+            if (value!.isEmpty) {
+              return "Phone Number is empty";
+            }
+            if (value.length < 11) {
+              return "Enter the correct mobile number";
+            }
+            return null;
+          },
+        );
+      },
     );
   }
 }
