004b0ffc9de5f240af9d73194f7be585533385a8
Naman2004singh <naman.2singh9@gmail.com>
2025-04-13 00:02:25 +0530

    updated the full name state with bloc and implement validations

--- lib/views/registration/widgets/full_name.dart
+++ lib/views/registration/widgets/full_name.dart
@@ -1,5 +1,5 @@
-import 'package:fashion_app/utils/input_decoration.dart';
 import 'package:flutter/material.dart';
+import 'common_exports.dart';
 
 class FullName extends StatelessWidget {
   const FullName({super.key, required this.nameFocusNode});
@@ -8,12 +8,28 @@ class FullName extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      keyboardType: TextInputType.name,
-      focusNode: nameFocusNode,
-      decoration: CustomInputDecoration.getDecoration(hintText: "Enter your Full Name"),
-      onChanged: (value) {},
-      validator: (value) {},
+    return BlocBuilder<RegisterBloc, RegisterStates>(
+      buildWhen: (previous, current) => previous.name != current.name,
+      builder: (context, state) {
+        return TextFormField(
+          keyboardType: TextInputType.name,
+          focusNode: nameFocusNode,
+          decoration: CustomInputDecoration.getDecoration(
+              hintText: "Enter your Full Name"),
+          onChanged: (value) {
+            context.read<RegisterBloc>().add(NameChanged(name: value));
+          },
+          validator: (value) {
+            if (value!.isEmpty) {
+              return "Name is Empty";
+            }
+            if (value.length < 4) {
+              return "Please enter your full name";
+            }
+            return null;
+          },
+        );
+      },
     );
   }
 }
